836f96e25da598cfeee37bd35fc79a5783f8da89
Megha Sudheer <33977948+meghamitthu@users.noreply.github.com>
2020-10-09 12:42:37 +0530

    Update overModel.js

--- Models/overModel.js
+++ Models/overModel.js
@@ -3,7 +3,7 @@ const {Schema} = mongoose;
 const Each_over = new Schema({
     team: String,
     over_number :{type:Number,min:1,max:20},
-    bowler: {type:Schema.Types.ObjectId, ref:'bowler'},
+    bowler: {type:Schema.Types.ObjectId, ref:'Bowl'},
     total_runs: Number,
     des:[
         {ball: Number,
